4f886c8a9324501192d7cabaddd890311faa0028
Adrian Albu <2412rock@gmail.com>
2024-12-25 16:50:58 +0200

    fixes

--- backup.bat
+++ backup.bat
@@ -4,7 +4,7 @@ $DBName = "FfhubDB"
 $BackupDir = "C:\Backups"  # Change to your desired backup directory
 $BackupFileName = "${DBName}_$(Get-Date -Format 'yyyyMMddHHmmss').bak"
 $BackupFilePath = Join-Path $BackupDir $BackupFileName
-$ContainerBackupPath = "/var/opt/mssql18/backups/$BackupFileName"
+$ContainerBackupPath = "/var/opt/mssql/$BackupFileName"
 
 # Retrieve the SQL password from the TeamCity parameter
 $SAPassword = "%sql_passwd%"  # This is where the password is passed as a parameter from TeamCity
@@ -16,17 +16,17 @@ if (!(Test-Path -Path $BackupDir)) {
 
 # Step 1: Run the backup command inside the Docker container
 Write-Host "Running backup inside the container..."
-docker exec $ContainerName /opt/mssql-tool18/bin/sqlcmd `
-    -S localhost -U SA -P $SAPassword `
+docker exec $ContainerName /opt/mssql-tools18/bin/sqlcmd `
+    -C -S localhost -U SA -P $SAPassword `
     -Q "BACKUP DATABASE [$DBName] TO DISK = N'$ContainerBackupPath' WITH NOFORMAT, INIT, NAME = '$DBName-full-backup', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
 
 # Step 2: Copy the backup file from the container to the host
 Write-Host "Copying backup file from container to host..."
-docker cp "$ContainerName:$ContainerBackupPath" $BackupFilePath
+docker cp "${ContainerName}:${ContainerBackupPath}" $BackupFilePath
 
 # Step 3: Clean up the backup file inside the container (optional)
-Write-Host "Cleaning up backup file inside the container..."
-docker exec $ContainerName rm $ContainerBackupPath
+#Write-Host "Cleaning up backup file inside the container..."
+#docker exec $ContainerName rm $ContainerBackupPath
 
 # Final message
 Write-Host "Backup completed successfully! Backup file saved to $BackupFilePath"
